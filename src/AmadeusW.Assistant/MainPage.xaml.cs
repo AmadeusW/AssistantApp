caec881fad4f14a0bc87aa97d8387e9c2d6053b4
Amadeusz Wieczorek <git@amadeusw.com>
2016-05-18 16:31:58 -0700

    Assistant universal app project

---
+++ src/AmadeusW.Assistant/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AmadeusW.Assistant
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SleepButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void PhotoButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void SwitchCameraButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void RepeatButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void ShareButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void RefineButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
